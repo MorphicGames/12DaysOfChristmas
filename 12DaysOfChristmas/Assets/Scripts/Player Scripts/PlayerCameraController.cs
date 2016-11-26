cbb4149fb5999e25a32f107dca6f42707547566f
Robert Sponagle <morphic.gaming.net@gmail.com>
2016-11-25 20:01:49 -0500

    Rob Update - 11/25/16
    + Added Player Camera Controller
    + Added Player Controller
    
    There is currently a small screen tearing issue, will need to fix

---
+++ 12DaysOfChristmas/Assets/Scripts/Player Scripts/PlayerCameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCameraController : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothingVector;
+
+    public float sensitivity;
+    public float smoothing;
+
+    GameObject playerCharacter;
+
+	// Use this for initialization
+	void Start () {
+        //Sets playerCharacter as the parent of the Camera Object
+        playerCharacter = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Camera Control Script
+        Vector2 mouseDirection = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDirection = Vector2.Scale(mouseDirection, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothingVector.x = Mathf.Lerp(smoothingVector.x, mouseDirection.x, 1.0f / smoothing);
+        smoothingVector.y = Mathf.Lerp(smoothingVector.y, mouseDirection.y, 1.0f / smoothing);
+        mouseLook += smoothingVector;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90.0f, 90.0f);
+
+        //Rotates Camera and Parent Body to match Horizontal and Vertical Changes
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        playerCharacter.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Vector3.up);
+	}
+}
