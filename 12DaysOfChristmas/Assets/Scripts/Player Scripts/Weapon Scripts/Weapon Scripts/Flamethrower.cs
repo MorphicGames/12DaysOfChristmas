89cf68b5bcc94c6bb0cca1cba0b1fa37e4bcb261
Edwin Chen <edwin14chen@gmail.com>
2016-12-07 17:38:00 -0500

    Flamethrower

--- 12DaysOfChristmas/Assets/Scripts/Player Scripts/Weapon Scripts/Weapon Scripts/Flamethrower.cs
+++ 12DaysOfChristmas/Assets/Scripts/Player Scripts/Weapon Scripts/Weapon Scripts/Flamethrower.cs
@@ -7,12 +7,19 @@ public class Flamethrower : Weapon {
 
     public override void Hide(bool toggle)
     {
-
+        if (toggle)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+        }
     }
 
     public override void Fire(PlayerInventory playerInv)
     {
-        
+        Flame.Play();
     }
 
 }
